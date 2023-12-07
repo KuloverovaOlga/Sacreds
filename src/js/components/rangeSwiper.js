e3b85c41cb853574772c221bbf9773e85888f919
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2023-12-07 23:40:08 +0400

    feat: update main page

--- src/js/components/rangeSwiper.js
+++ src/js/components/rangeSwiper.js
@@ -4,58 +4,175 @@ import { rem } from "../utils/constants";
 
 
 
-const rangeSwiper = () => {
- 
-      // Initialize Swiper
-
-
-      const swiper = new Swiper('.range__swiper', {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        direction: 'horizontal',
-        scrollbar: {
-          el: '.range__swiper-scrollbar',
-          dragSize: rem(11.3),
-          draggable: true,
-        },
-      });
+// const rangeSwiper = () => {
+//       const swiper = new Swiper('.range__swiper', {
+//         autoHeight: true,
+//         slidesPerView: 1,
+//         spaceBetween: 0,
+//         direction: 'horizontal',
+//         scrollbar: {
+//           el: '.range__swiper-scrollbar',
+//           dragSize: rem(11.3),
+//           // draggable: true,
+//         },
+//       });
 
       
 
-      // Navigation Menu
-      const navItems = document.querySelectorAll('.range__swiper-nav-item');
-      const scrollbarDrag = document.querySelector('.range__swiper-scrollbar-drag');
+//       // Navigation Menu
+//       const navItems = document.querySelectorAll('.range__swiper-nav-item');
+//       const scrollbarDrag = document.querySelector('.range__swiper-scrollbar-drag');
 
 
 
-      navItems.forEach((item, index) => {
+//       navItems.forEach((item, index) => {
 
-        item.addEventListener('click', function () {
-          swiper.slideTo(index);
+//         item.addEventListener('click', function () {
+//           swiper.slideTo(index);
 
-          // Set width of the scrollbar drag to match the selected menu item
-          const width = item.offsetWidth;
-          const left = item.offsetLeft;
-          scrollbarDrag.style.width = `${width}px`;
-          scrollbarDrag.style.transform = `translateX(${left}px)`;
+//           // Set width of the scrollbar drag to match the selected menu item
 
-          navItems.forEach(item => {
-            item.classList.remove('isActive')
-          })
-          item.classList.add('isActive')
+          
+//           const width = item.offsetWidth;
+//           const left = item.offsetLeft;
+//           scrollbarDrag.style.width = `${width}px`;
+//           scrollbarDrag.style.transform = `translateX(${left}px)`;
 
-        });
+//           navItems.forEach(item => {
+//             item.classList.remove('isActive')
+//           })
+//           item.classList.add('isActive')
+
+//         });
+//       });
+
+//       // Sync Swiper with Scrollbar
+//       swiper.on('scroll', function () {
+//         const progress = swiper.progress;
+//         const maxTranslate = scrollbarDrag.parentElement.offsetWidth - scrollbarDrag.offsetWidth;
+//         const translateX = maxTranslate * progress;
+//         scrollbarDrag.style.transform = `translateX(${translateX}px)`;
+//       });
+
+
+// }
+
+// const rangeSwiper = () => {
+//   const swiper = new Swiper('.range__swiper', {
+//     autoHeight: true,
+//     slidesPerView: 1,
+//     spaceBetween: 0,
+//     direction: 'horizontal',
+//     scrollbar: {
+//       el: '.range__swiper-scrollbar',
+//       dragSize: rem(11.3),
+//     },
+//   });
+
+//   // Navigation Menu
+//   const navItems = document.querySelectorAll('.range__swiper-nav-item');
+//   const scrollbarDrag = document.querySelector('.range__swiper-scrollbar-drag');
+
+//   navItems.forEach((item, index) => {
+//     item.addEventListener('click', function () {
+//       swiper.slideTo(index);
+
+//       // Set width of the scrollbar drag to match the selected menu item
+//       const width = item.offsetWidth;
+//       const left = item.offsetLeft;
+//       scrollbarDrag.style.width = `${width}px`;
+//       scrollbarDrag.style.transform = `translateX(${left}px)`;
+
+//       navItems.forEach((item) => {
+//         item.classList.remove('isActive');
+//       });
+//       item.classList.add('isActive');
+//     });
+//   });
+
+//   // Sync Swiper with Scrollbar
+//   swiper.on('slideChange', function () {
+//     const activeNavItem = document.querySelector('.range__swiper-nav-item.isActive');
+//     if (activeNavItem) {
+//       activeNavItem.classList.remove('isActive');
+//     }
+
+//     const currentSlideIndex = swiper.activeIndex;
+//     const newActiveNavItem = navItems[currentSlideIndex];
+//     newActiveNavItem.classList.add('isActive');
+
+//     // Set width of the scrollbar drag to match the selected menu item
+//     const width = newActiveNavItem.offsetWidth;
+//     const left = newActiveNavItem.offsetLeft;
+//     scrollbarDrag.style.width = `${width}px`;
+//     scrollbarDrag.style.transform = `translateX(${left}px)`;
+//   });
+// };
+
+
+const rangeSwiper = () => {
+  const swiper = new Swiper('.range__swiper', {
+    // autoHeight: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    direction: 'horizontal',
+    scrollbar: {
+      el: '.range__swiper-scrollbar',
+      dragSize: rem(11.3),
+    },
+  });
+
+
+  // Navigation Menu
+  const navItems = document.querySelectorAll('.range__swiper-nav-item');
+  const scrollbarDrag = document.querySelector('.range__swiper-scrollbar-drag');
+
+  navItems.forEach((item, index) => {
+    item.addEventListener('click', function () {
+      swiper.slideTo(index);
+
+      // Set width of the scrollbar drag to match the selected menu item
+      const width = item.offsetWidth;
+      const left = item.offsetLeft;
+      scrollbarDrag.style.width = `${width}px`;
+      scrollbarDrag.style.transform = `translateX(${left}px)`;
+
+      navItems.forEach((item) => {
+        item.classList.remove('isActive');
       });
+      item.classList.add('isActive');
+    });
+  });
 
-      // Sync Swiper with Scrollbar
-      swiper.on('scroll', function () {
-        const progress = swiper.progress;
-        const maxTranslate = scrollbarDrag.parentElement.offsetWidth - scrollbarDrag.offsetWidth;
-        const translateX = maxTranslate * progress;
-        scrollbarDrag.style.transform = `translateX(${translateX}px)`;
-      });
+  // Sync Swiper with Scrollbar
+  swiper.on('slideChange', function () {
+    const activeNavItem = document.querySelector('.range__swiper-nav-item.isActive');
+    if (activeNavItem) {
+      activeNavItem.classList.remove('isActive');
+    }
 
 
-}
+    const currentSlideIndex = swiper.activeIndex;
+    const newActiveNavItem = navItems[currentSlideIndex];
+    newActiveNavItem.classList.add('isActive');
+
+    // Set width of the scrollbar drag to match the selected menu item
+    const width = newActiveNavItem.offsetWidth;
+    const left = newActiveNavItem.offsetLeft;
+    scrollbarDrag.style.width = `${width}px`;
+
+    // Calculate the maximum translateX to keep the scrollbarDrag within the scrollbar
+    const maxTranslateX = scrollbarDrag.parentElement.offsetWidth - scrollbarDrag.offsetWidth;
+    const translateX = Math.min(left, maxTranslateX);
+    scrollbarDrag.style.transform = `translateX(${translateX}px)`;
+  });
+};
+
+
+
+
+
+
+
 
 export default rangeSwiper
